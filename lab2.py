47cc7d0b623bfe373d351f2f02f93c93152878e9
Vlad Markin <vlad.d.markin@gmail.com>
2018-03-15 14:58:53 +0300

    Lab 2 calc centers for connecting arcs

--- lab2.py
+++ lab2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 import numpy as np
 import math
-import random
+from shapely.geometry import LineString, LinearRing, MultiLineString
 
 # page
 # тест криволинейной фигурой эллипсом и ниже в главе
@@ -42,8 +42,11 @@ plt.axis([-12, 12, -12, 12])
 
 #ax = plt.axes([0, 0.25, 0.65, 1])
 
+arc_R = 3
+
 circle_R = 4
-circle_points = circle(np.array([0, 0]), circle_R)
+circle_center = np.array([0, 0])
+circle_points = circle(circle_center, circle_R)
 lines_circle, = ax.plot(*circle_points.transpose())
 ax.set_aspect('equal')
 
@@ -52,20 +55,9 @@ sl_circleR = Slider(ax_circleR, 'Circle R', 0.5, 10, valinit=circle_R)
 
 
 ax_arcR = plt.axes([0.6, 0.9, 0.3, 0.04])
-sl_arcR = Slider(ax_arcR, 'Arc R', 1, 10)
+sl_arcR = Slider(ax_arcR, 'Arc R', 1, 10, valinit=arc_R)
 
-
-def update_c(v):
-    global circle_points
-    global circle_R
-    circle_R = sl_circleR.val
-    circle_points = circle(np.array([0, 0]), circle_R)
-    lines_circle.set_data(*circle_points.transpose())
-    fig.canvas.draw_idle()
-
-
-sl_circleR.on_changed(update_c)
-
+shape_circle = LinearRing(circle(circle_center, circle_R - arc_R))
 
 pt_A = np.array([0, 0])
 pt_B = np.array([1, 1])
@@ -83,9 +75,26 @@ sl_lineBx = Slider(ax_lineBx, 'Bx', -10, 10, valinit=pt_B[0])
 sl_lineBy = Slider(ax_lineBy, 'By', -10, 10, valinit=pt_B[1])
 
 
-def update_l(v):
-    global line_points
+shape_line = LineString(line_points)
+shape_line_left = shape_line.parallel_offset(arc_R, 'left')
+shape_line_right = shape_line.parallel_offset(arc_R, 'right')
+
+lines_intersect, = ax.plot([], marker='o', color='r', ls='')
+
+def update(v):
+    global circle_points
+    global circle_R, arc_R
+    global line_points, shape_line_left, shape_line_right
     global pt_A, pt_B
+    # Update arc radius
+    arc_R = sl_arcR.val
+    # Update circle
+    circle_R = sl_circleR.val
+    circle_points = circle(np.array([0, 0]), circle_R)
+    lines_circle.set_data(*circle_points.transpose())
+    fig.canvas.draw_idle()
+    shape_circle.coords = circle(circle_center, circle_R - arc_R)
+    # Update lines
     pt_A[0] = sl_lineAx.val
     pt_A[1] = sl_lineAy.val
     pt_B[0] = sl_lineBx.val
@@ -93,11 +102,24 @@ def update_l(v):
     line_points = line(pt_A, pt_B, -12, 12)
     lines_line.set_data(*line_points.transpose())
     fig.canvas.draw_idle()
+    shape_line.coords = line_points
+    shape_line_left = shape_line.parallel_offset(arc_R, 'left')
+    shape_line_right = shape_line.parallel_offset(arc_R, 'right')
+    # Calculate intersections
+    intersection = shape_circle.intersection(MultiLineString([shape_line_left, shape_line_right]))
+    inter_coords = []
+    for pt in list(intersection):
+        inter_coords.append(pt.coords)
+    if len(inter_coords):
+        print('intersect')
+        lines_intersect.set_data(*np.array(inter_coords).transpose())
 
+sl_circleR.on_changed(update)
+sl_arcR.on_changed(update)
 
-sl_lineAx.on_changed(update_l)
-sl_lineAy.on_changed(update_l)
-sl_lineBx.on_changed(update_l)
-sl_lineBy.on_changed(update_l)
+sl_lineAx.on_changed(update)
+sl_lineAy.on_changed(update)
+sl_lineBx.on_changed(update)
+sl_lineBy.on_changed(update)
 
 plt.show()
